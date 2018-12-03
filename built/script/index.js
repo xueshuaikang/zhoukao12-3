8770ecd1b73ca2f1dd475f4f8a1871f09a7e98fd
xuenuo <626096325@qq.com>
2018-12-03 19:46:31 +0800

    起服务成功

--- built/script/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";for(var num=0,len=20,i=0;i<len;i++)console.log(i);function con(){for(var o=0;o<len;o++)console.log(o);for(var n=[1,2,3,4,5,6,7,8],l=0;l<n.length;l++)console.log(l)}con();
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: mikey.薛帅康 
+ * @Date: 2018-12-03 19:31:10 
+ * @Last Modified by: mikey.薛帅康
+ * @Last Modified time: 2018-12-03 19:45:59
+ */
 let gulp=require("gulp");
 
 var sass=require("gulp-sass");
@@ -14,6 +20,12 @@ let babel=require("gulp-babel");
 
 let server=require("gulp-webserver");
 
+let url=require("url");
+
+let fs=require("fs");
+
+let path=require("path");
+
 // 编译scss  合并  压缩
 gulp.task("readscss",()=>{
     return gulp.src("./src/style/*.scss")
@@ -25,11 +37,36 @@ gulp.task("readscss",()=>{
 
 // 压缩js文件
 gulp.task("minjs",()=>{
-    return gulp.src("./src/script/index.js")
+    return gulp.src("./src/script/*.js")
     .pipe(babel({
         presets: ['@babel/env']
     }))
+    .pipe(concat("all.js"))
     .pipe(minjs())
     .pipe(gulp.dest("./built/script"))
 })
 
+// 起服务
+gulp.task("server",()=>{
+    return gulp.src('./src')
+    .pipe(server({
+        port:9900,
+        open: true,
+        //fallback: 'index.html',
+        //host:"169.254.76.137",
+        // middleware:function(req,res,next){
+        //      // 获取地址栏 localhost 后面的地址路径的方法（属于原生 node）
+        //      let pathname=url.parse(req.url).pathname;
+
+        //      if(pathname=="/favicon.ico"){
+        //          return;
+        //      }
+        // }
+    }));
+})
+
+// gulp.task("watch",()=>{
+//     return gulp.watch()
+// })
+
+gulp.task("default",gulp.series("readscss","minjs"))
